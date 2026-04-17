e287a0952e52dc7da5f9d4a807e4495d5845db29
Nikolos Hurrinus <potatomasterme@gmail.com>
2026-04-16 22:52:44 -0400

    initial part working, partial part 2

--- src/main/java/aydin/firebasedemo/SecondaryController.java
+++ src/main/java/aydin/firebasedemo/SecondaryController.java
@@ -1,12 +1,56 @@
 package aydin.firebasedemo;
 
 import java.io.IOException;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private Button registerButton;
+
+    @FXML
+    private Button signInButton;
+
+
+    @FXML
+    private void signIn() throws IOException {
         DemoApp.setRoot("primary");
     }
+
+    @FXML
+    void registerButtonClicked(ActionEvent event) {
+        registerUser();
+    }
+
+
+
+    public boolean registerUser() {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail("user222@example.com")
+                .setEmailVerified(false)
+                .setPassword("secretPassword")
+                .setPhoneNumber("+11234567890")
+                .setDisplayName("John Doe")
+                .setDisabled(false);
+
+        UserRecord userRecord;
+        try {
+            userRecord = DemoApp.fauth.createUser(request);
+            System.out.println("Successfully created new user with Firebase Uid: " + userRecord.getUid()
+                    + " check Firebase > Authentication > Users tab");
+            return true;
+
+        } catch (FirebaseAuthException ex) {
+            // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error creating a new user in the firebase");
+            return false;
+        }
+
+    }
+
 }
